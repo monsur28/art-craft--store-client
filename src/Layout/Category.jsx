dfd4694c7d6df6d56a47d375c21694439ccf9194
Abul Monsur <154686044+monsur28@users.noreply.github.com>
2024-04-30 19:13:53 +0600

    Category Added

--- src/Layout/Category.jsx
+++ src/Layout/Category.jsx
@@ -1,20 +1,24 @@
 import { useEffect, useState } from "react";
 import { Typewriter } from "react-simple-typewriter";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 import CategorySection from "./CategorySection";
 const Category = () => {
-  const [categories, setCategories] = useState([]);
+  const [artData, setArtData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
   useEffect(() => {
     fetch("http://localhost:5000/art")
       .then((res) => res.json())
-      .then((data) => {
-        const uniqueCategories = data.filter(
-          (data) => data.subCategoryName === data.subCategoryName
-        );
-        setCategories(uniqueCategories);
-      });
+      .then((data) => setArtData(data));
   }, []);
-  console.log(categories);
+
+  // Filter data based on category
+  const filteredData =
+    selectedCategory === "All"
+      ? artData
+      : artData.filter((item) => item.subcategoryName === selectedCategory);
   return (
     <div>
       <div className="text-center text-4xl">
@@ -44,73 +48,75 @@ const Category = () => {
           </span>
         </h1>
       </div>
-      <div className="grid grid-cols-3 gap-4">
-          {categories.map((item) => (
-            <CategorySection key={item._id} item={item}></CategorySection>
-          ))}
-          {/* <div className="card bg-base-100 shadow-xl image-full">
-            <figure>
-              <img
-                src="https://i.ibb.co/Tm02Vss/The-Starry-Night-1200x630-1.jpg"
-                alt="Landscape Painting"
-              />
-            </figure>
-            <div className="card-body flex justify-center items-center">
-              <h2 className="card-title">Landscape Painting</h2>
+      <div className="mt-6 max-h-fit">
+        <Tabs>
+          <TabList>
+            <Tab onClick={() => setSelectedCategory("All")}>All</Tab>
+            <Tab onClick={() => setSelectedCategory("Landscape Painting")}>
+              Single-family homes
+            </Tab>
+            <Tab onClick={() => setSelectedCategory("Portrait Drawing")}>
+              Apartment
+            </Tab>
+            <Tab onClick={() => setSelectedCategory("Watercolour Painting")}>
+              Townhouses
+            </Tab>
+            <div onClick={() => setSelectedCategory("Oil Painting")}>
+              Senior living communities
             </div>
-          </div> */}
-        {/* <div className="card bg-base-100 shadow-xl image-full">
-          <figure>
-            <img src="https://i.ibb.co/2S3HpJb/Rod-Luff-006.jpg" alt="Shoes" />
-          </figure>
-          <div className="card-body flex justify-center items-center">
-            <h2 className="card-title">Portrait Drawing</h2>
-          </div>
-        </div>
-        <div className="card bg-base-100 shadow-xl image-full">
-          <figure>
-            <img
-              src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a39f2b2a-6368-44ed-bddd-634d02fd9378/dgn91kg-282ed38f-5509-4e76-9bcb-6f5a8b15fcad.png/v1/fill/w_1280,h_718,q_80,strp/ethereal_dawn___fiery_landscape_art_print_by_mitoarts_dgn91kg-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzE4IiwicGF0aCI6IlwvZlwvYTM5ZjJiMmEtNjM2OC00NGVkLWJkZGQtNjM0ZDAyZmQ5Mzc4XC9kZ245MWtnLTI4MmVkMzhmLTU1MDktNGU3Ni05YmNiLTZmNWE4YjE1ZmNhZC5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.0qR4r9LY041J7-2M2S5tAqd8oZs4bOFYiJwnHwMzDDU"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body flex justify-center items-center">
-            <h2 className="card-title">Oil Painting</h2>
-          </div>
-        </div>
-        <div className="card bg-base-100 shadow-xl image-full ">
-          <figure>
-            <img
-              src="https://www.rileystreet.com/cdn/shop/articles/shutterstock_410271079_1024x1024.jpg?v=1624398205"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body flex justify-center items-center">
-            <h2 className="card-title">Watercolour Painting:</h2>
-          </div>
-        </div>
-        <div className="card bg-base-100 shadow-xl image-full">
-          <figure>
-            <img
-              src="https://blog.daisie.com/content/images/size/w2000/2023/03/birmingham-museums-trust-gHkgqZhHPbM-unsplash.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body flex justify-center items-center">
-            <h2 className="card-title">Charcoal Sketching</h2>
-          </div>
-        </div>
-        <div className="card bg-base-100 shadow-xl image-full">
-          <figure>
-            <img
-              src="https://cooldrawingidea.com/wp-content/uploads/2020/05/Doraemon-Drawing.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body flex justify-center items-center">
-            <h2 className="card-title">Cartoon Drawing</h2>
-          </div>
-        </div> */}
+            <Tab onClick={() => setSelectedCategory("Charcoal Sketching")}>
+              Student housing
+            </Tab>
+            <Tab onClick={() => setSelectedCategory("Cartoon Drawing")}>
+              Vacation rentals
+            </Tab>
+          </TabList>
+          <TabPanel>
+            <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4"></ul>
+          </TabPanel>
+          <TabPanel>
+            <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+              {filteredData.map((data) => (
+                <CategorySection key={data.id} data={data}></CategorySection>
+              ))}
+            </ul>
+          </TabPanel>
+          <TabPanel>
+            <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+              {filteredData.map((data) => (
+                <CategorySection key={data.id} data={data}></CategorySection>
+              ))}
+            </ul>
+          </TabPanel>
+          <TabPanel>
+            <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+              {filteredData.map((data) => (
+                <CategorySection key={data.id} data={data}></CategorySection>
+              ))}
+            </ul>
+          </TabPanel>
+          <TabPanel>
+            <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+              {filteredData.map((data) => (
+                <CategorySection key={data.id} data={data}></CategorySection>
+              ))}
+            </ul>
+          </TabPanel>
+          <TabPanel>
+            <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+              {filteredData.map((data) => (
+                <CategorySection key={data.id} data={data}></CategorySection>
+              ))}
+            </ul>
+          </TabPanel>
+          <TabPanel>
+            <ul className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+              {filteredData.map((data) => (
+                <CategorySection key={data.id} data={data}></CategorySection>
+              ))}
+            </ul>
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
   );
--- src/Layout/CategorySection.jsx
+++ src/Layout/CategorySection.jsx
@@ -1,19 +1,17 @@
 /* eslint-disable react/prop-types */
-const CategorySection = ({ item }) => {
-  const { photoURL, subcategoryName } = item;
+const CategorySection = ({ data }) => {
+  const { photoURL, subcategoryName } = data;
   return (
-    <Link to={`${subcategoryName}`}>
-      <div className="cursor-pointer border rounded-md">
-        <div
-          className="p-5 flex justify-center bg-cover rounded-md"
-          style={{ backgroundImage: `url('${photoURL}')` }}
-        >
-          <h2 className="card-title my-10 text-[14px] border bg-[#FFFFFF] px-[10px] py-2 rounded-md">
-            {subcategoryName}
-          </h2>
-        </div>
+    <div className="cursor-pointer border rounded-md">
+      <div
+        className="p-5 flex justify-center bg-cover rounded-md"
+        style={{ backgroundImage: `url('${photoURL}')` }}
+      >
+        <h2 className="card-title my-10 text-[14px] border bg-[#FFFFFF] px-[10px] py-2 rounded-md">
+          {subcategoryName}
+        </h2>
       </div>
-    </Link>
+    </div>
   );
 };
 
